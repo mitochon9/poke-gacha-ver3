043b1b87ec5623de366e621b3657fee5453b6fdd
mitochon9 <mitochon9@gmail.com>
2022-02-25 23:09:21 +0900

    ✨ Button Atom の追加

---
+++ src/component/atom/Button/Button.stories.tsx
@@ -0,0 +1,56 @@
+import { ComponentMeta, Story } from '@storybook/react';
+import { Button, ButtonProps } from './Button';
+
+export default {
+  title: 'atom/Button',
+  component: Button,
+} as ComponentMeta<typeof Button>;
+
+const Template: Story<ButtonProps> = (args) => <Button {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  children: '',
+};
+
+export const ButtonA = Template.bind({});
+ButtonA.args = {
+  children: 'A',
+  variant: 'rounded',
+};
+export const ButtonB = Template.bind({});
+ButtonB.args = {
+  children: 'B',
+  variant: 'rounded',
+};
+
+export const CrossUp = Template.bind({});
+CrossUp.args = {
+  children: '▲',
+  variant: 'square',
+};
+export const CrossDown = Template.bind({});
+CrossDown.args = {
+  children: '▼',
+  variant: 'square',
+};
+export const CrossLeft = Template.bind({});
+CrossLeft.args = {
+  children: '◀',
+  variant: 'square',
+};
+export const CrossRight = Template.bind({});
+CrossRight.args = {
+  children: '▶',
+  variant: 'square',
+};
+
+export const ButtonStart = Template.bind({});
+ButtonStart.args = {
+  variant: 'ellipse',
+};
+
+export const ButtonSelect = Template.bind({});
+ButtonSelect.args = {
+  variant: 'ellipse',
+};
