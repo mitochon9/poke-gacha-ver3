d687857b5d9cedba1a323343f6ff36bc1d84d7d5
mitochon9 <mitochon9@gmail.com>
2022-02-27 14:34:48 +0900

    🩹 軽微な修正

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,3 @@
-import { AbButton } from '@/component/molecule/AbButton';
-
 const Home: React.FC = () => (
   <main className='mx-auto max-w-6xl py-6'>
     <h1 key='index-page' className='text-lg font-bold text-primary-800'>
