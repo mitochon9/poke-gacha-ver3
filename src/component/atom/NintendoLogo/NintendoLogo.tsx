cbd1f01b63a472062cf194af4b8c1e98a42340e5
mitochon9 <mitochon9@gmail.com>
2022-02-27 12:04:11 +0900

    ✨ Nintendo ロゴの作成

---
+++ src/component/atom/NintendoLogo/NintendoLogo.tsx
@@ -0,0 +1,12 @@
+export const NintendoLogo: React.FC = () => (
+  <>
+    <div className='mt-6 text-center'>
+      <div
+        key='nintendo-logo'
+        className='inline-block rounded-full border-2 border-gray-700 px-2 font-bold italic tracking-widest text-gray-700'
+      >
+        Nintendo
+      </div>
+    </div>
+  </>
+);
